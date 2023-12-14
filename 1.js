4614336a8bfdb7119c08e545b0f01b5d4e19541d
katarilp <katarilp@metropolia.fi>
2023-12-14 19:25:59 +0200

    Initial commit

---
+++ 1.js
@@ -0,0 +1,11 @@
+'use strict';
+const element = document.querySelector('.my-list');
+element.innerHTML += '<li>First item</li>';
+element.innerHTML += '<li>Second item</li>';
+element.innerHTML += '<li>Third item</li>';
+
+
+/* Open t1 folder in your IDE/editor. Add HTML by using innerHTML property (2p)
+Add the following HTML code to the element with id="target"
+
+Add class my-list to the element with id="target" */
